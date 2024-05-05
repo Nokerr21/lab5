c994c4d273c6efdb58a862accac00db25de06410
Radosław <radoslaw.godlewski00@gmail.com>
2024-05-05 17:59:17 +0200

    Changed test class, added plots

--- Lab4/main.py
+++ Lab4/main.py
@@ -1,7 +1,18 @@
 import ID3
 import numpy as np
 import pandas as pd
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
+import matplotlib.pyplot as plt
 
+
+def prepare_confusion_matrix(real_data, predicted_data, labels):
+    matrix = confusion_matrix(real_data, predicted_data)
+    sns.heatmap(matrix, annot=True, fmt="d", cmap="Blues", xticklabels=labels, yticklabels=labels)
+    plt.xlabel("Przewidywane")
+    plt.ylabel("Rzeczywiste")
+    plt.title("Macierz Pomyłek")
+    plt.show()
 
 def read_dataset_from_file(file):
     return pd.read_csv(file)
@@ -47,8 +58,7 @@ def perform_mushrooms_training_and_test():
     decision_tree = ID3.ID3Node(training_set, attributes)
     test_set.reset_index()
 
-    print(perform_decision_tree_test(decision_tree, test_set) / len(test_set))
-
+    return perform_decision_tree_test(decision_tree, test_set)
 
 def perform_breast_cancer_training_and_test():
     breast_cancer_dataset = read_dataset_from_file('breast-cancer.data')
@@ -68,7 +78,7 @@ def perform_breast_cancer_training_and_test():
     decision_tree = ID3.ID3Node(training_set, attributes)
     test_set.reset_index()
 
-    print(perform_decision_tree_test(decision_tree, test_set) / len(test_set))
+    return perform_decision_tree_test(decision_tree, test_set)
 
 
 def divide_data_set(data_set):
@@ -82,11 +92,46 @@ def divide_data_set(data_set):
 
 def perform_decision_tree_test(decision_tree, test_set):
     counter = 0
+    predictions = []
     for index, row in test_set.iterrows():
-        if row['Class'] == decision_tree.predict(row[1:]):
+        prediction = decision_tree.predict(row[1:])
+        predictions.append(prediction)
+        if row['Class'] == prediction:
             counter += 1
-    return counter
+    labels = test_set['Class'].unique().tolist()
+    return counter, test_set['Class'].tolist(), predictions, labels
 
 
-perform_mushrooms_training_and_test()
-perform_breast_cancer_training_and_test()
+def perform_tests(number_of_runs):
+    mushrooms_accuracy = 0
+    mushrooms_test_set = []
+    mushrooms_predict_set = []
+    mushrooms_labels = []
+    breast_cancer_accuracy = 0
+    breast_cancer_test_set = []
+    breast_cancer_predict_set = []
+    breast_cancer_labels = []
+    for i in range(number_of_runs):
+        mushrooms_results = perform_mushrooms_training_and_test()
+        mushrooms_accuracy += mushrooms_results[0]
+        mushrooms_test_set.extend(mushrooms_results[1])
+        mushrooms_predict_set.extend(mushrooms_results[2])
+        mushrooms_labels = mushrooms_results[3]
+
+        breast_cancer_results = perform_breast_cancer_training_and_test()
+        breast_cancer_accuracy += breast_cancer_results[0]
+        breast_cancer_test_set.extend(breast_cancer_results[1])
+        breast_cancer_predict_set.extend(breast_cancer_results[2])
+        breast_cancer_labels = breast_cancer_results[3]
+
+    mushrooms_average_accuracy = mushrooms_accuracy / len(mushrooms_test_set)
+    prepare_confusion_matrix(mushrooms_test_set, mushrooms_predict_set, mushrooms_labels)
+    print(mushrooms_average_accuracy)
+
+    breast_cancer_average_accuracy = breast_cancer_accuracy / len(breast_cancer_test_set)
+    prepare_confusion_matrix(breast_cancer_test_set, breast_cancer_predict_set, breast_cancer_labels)
+    print(breast_cancer_average_accuracy)
+
+
+NUMBER_OF_RUNS = 20
+perform_tests(NUMBER_OF_RUNS)
